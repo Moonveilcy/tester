41aa2b038c176515fc9aa8a134d147f47e3b0bf8
Gaeuly <7dsgaeul@gmail.com>
2025-10-20 03:37:22 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
@@ -7,18 +7,16 @@ import ReadmePage from './pages/ReadmePage';
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-white">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/commit" element={<CommitPage />} />
-            <Route path="/readme" element={<ReadmePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen bg-white">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/commit" element={<CommitPage />} />
+          <Route path="/readme" element={<ReadmePage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
